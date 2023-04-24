1795d58291bab2b41743cd9ecc6980e8dc55224a
georgyrudnev <georgy.rudnev@outlook.de>
2023-04-24 19:44:18 +0200

    Edited edit button and created new page

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    document.getElementById("Edit2").onclick = function () {
+        location.href = "Edit.html";
+    };
+
+    document.getElementById("Edit3").onclick = function () {
+        location.href = "Edit.html";
+    };
+
+
+}
